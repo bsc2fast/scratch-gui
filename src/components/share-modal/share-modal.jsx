dfe616c68bda25eefe1a6c3deed9b1e5ec1b7a4b
hsur753 <hsur753@aucklanduni.ac.nz>
2021-03-03 18:14:55 +1300

    modal elements and styles

--- src/components/share-modal/share-modal.jsx
+++ src/components/share-modal/share-modal.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Modal from '../../containers/modal.jsx';
 import Box from '../box/box.jsx';
 import styles from './share-modal.css';
+import {FormattedMessage} from 'react-intl';
 
 const ShareModal = props => (
     <Modal
@@ -14,7 +15,61 @@ const ShareModal = props => (
     >
         <div>
             <Box className={styles.body}>
+                <div>
+                    <p className={styles.joinFlowInputTitle}>
+                        <FormattedMessage
+                            defaultMessage="Project Name"
+                            description="Label for project name"
+                            id="gui.shareProject.projectName"
+                        />
+                    </p>
+                    <input
+                        name="projectName"
+                        className={styles.field}
+                        autoCapitalize="off"
+                        autoComplete="off"
+                        autoCorrect="off"
+                        id="projectName"
+                        placeholder="Project Name"
+                        spellCheck="false"
+                    />
+                </div>
+
+                <div>
+                    <p className={styles.joinFlowInputTitle}>
+                        <FormattedMessage
+                            defaultMessage="Author Name"
+                            description="Label for author name"
+                            id="gui.shareProject.authorName"
+                        />
+                    </p>
+                    <input
+                        name="authorName"
+                        className={styles.field}
+                        autoCapitalize="off"
+                        autoComplete="off"
+                        autoCorrect="off"
+                        id="authorName"
+                        placeholder="Author Name"
+                        spellCheck="false"
+                    />
+
+                </div>
+
             </Box>
+            <div>
+                <button
+                    className={styles.modalFlushBottomButton}
+                    id="projectUpload"
+                    type="submit"
+                >
+                    <FormattedMessage
+                        defaultMessage="Share!"
+                        description="Label for share project"
+                        id="gui.shareProject.shareProjectFile"
+                    />
+                </button>
+            </div>
         </div>
 
     </Modal>
