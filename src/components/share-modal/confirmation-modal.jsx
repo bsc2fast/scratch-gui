5bf81d7c62cebc2d61a635f302a0dcad5b1497b1
hsur753 <hsur753@aucklanduni.ac.nz>
2021-09-16 14:26:32 +1200

    confirmation modal functionality

--- src/components/share-modal/confirmation-modal.jsx
+++ src/components/share-modal/confirmation-modal.jsx
@@ -17,16 +17,65 @@ const ConfirmationModal = props => (
         <div>
             <Box className={styles.body}>
                 <div>
-                    <p className={styles.joinFlowInputTitle}>
+                    <p className={styles.challengeTitle}>
                         <FormattedMessage
-                            defaultMessage="Project Submitted!"
+                            defaultMessage="Thanks for Submitting!"
                             description="Label for confirmation"
                             id="gui.shareProject.projectSubmitted"
                         />
                     </p>
                 </div>
 
+                <div>
+                    <p className={styles.joinFlowInputTitle}>
+                        <FormattedMessage
+                            defaultMessage="We have received your project and will let you know the outcome
+                            during November 2021."
+                            description="Label for confirmation"
+                            id="gui.shareProject.confirmationDesc"
+                        />
+
+                        <br />
+                        <br />
+
+                        <FormattedMessage
+                            defaultMessage="Visit our "
+                            description="Label for share agree"
+                            id="gui.shareProject.confirmation1"
+                        />
+                        <a
+                            className={styles.challengeDescription}
+                            href="https://ahlab.org"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <FormattedMessage
+                                defaultMessage="website"
+                                description="Label for challenge website"
+                                id="gui.shareProject.website"
+                            />
+                        </a>
+                        <FormattedMessage
+                            defaultMessage=" to try out other projects and vote for your favourite!"
+                            description="Label for share agree2"
+                            id="gui.shareProject.confirmation2"
+                        />
+                    </p>
+                </div>
             </Box>
+            <div>
+                <button
+                    className={styles.modalFlushBottomButton}
+                    type="submit"
+                    onClick={props.onCancel}
+                >
+                    <FormattedMessage
+                        defaultMessage="Close"
+                        description="Label for close modal"
+                        id="gui.shareProject.confirmationClose"
+                    />
+                </button>
+            </div>
         </div>
 
     </Modal>
